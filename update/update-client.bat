d22b1f9bd28f9ad350c920f03fd25db75e6915c8
AzureRikaZangetsu <58664265+AzureRikaZangetsu@users.noreply.github.com>
2019-12-08 12:48:01 -0500

    Update update-client.bat

--- update/update-client.bat
+++ update/update-client.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 if exist update_new.exe move /y update_new.exe update.exe
-start update.exe -d "%~dp0MagicSetEditor2-NI" "https://https://github.com/AzureRikaZangetsu/Yu-Gi-Oh-MSE-Azure-Series-10/tree/master/"
+start update.exe -d "%~dp0MagicSetEditor2-NI" "https://github.com/AzureRikaZangetsu/Yu-Gi-Oh-MSE-Azure-Series-10/"
 exit
